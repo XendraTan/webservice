056a77d5466c1bd4107cc8cc15dde97365e16d62
XendraTan <23026180@myrp.edu.sg>
2026-01-20 10:04:02 +0800

    add of card

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const dbConfig = {
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit:100,
-    queuelimit: 0,
 };
 
 // initialize Express app
@@ -24,6 +23,55 @@ app.use(express.json());
 // start the server
 app.listen(port, () => {
     console.log('Server running on port',port);
+});
+
+//Example Route: Get all cards
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for allcards' });
+    }
+});
+
+app.post('/addcard', async (req, res) => {
+    const{ card_name, card_pic} = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?,?)', [card_name, card_pic]);
+        res.status(201).json({message: 'Card '+card_name+' added successfully.'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not add card '+card_name});
+    }
+});
+
+app.delete('/deletecard/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('DELETE FROM cards WHERE id=' + id);
+        res.status(201).json({message: 'Card ' + id + ' deleted successfully.'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not delete card '+id});
+    }
+});
+
+app.put('/updatecard/:id', async (req, res) => {
+    const { id } = req.params;
+    const { card_name, card_pic } = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('UPDATE cards SET card_name=?, card_pic=? WHERE id=?', [card_name, card_pic, id]);
+        res.status(201).json({message: 'Card '+card_name+' updated successfully.'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not update card '+id});
+    }
 });
 
 // route: get all movies
